74691db33b8c6e7f2a1712b7c785b0f56bdf77b3
Iouri Goussev <i.gouss@gmail.com>
2015-02-14 17:47:28 -0500

    Avoid setting hostname if it has not changed

--- sethostname.sh
+++ sethostname.sh
@@ -1,5 +1,11 @@
 OLD_HOSTNAME=`hostname`
 NEW_HOSTNAME=`/sbin/ifconfig  | grep 192.168.1 | head -1 | cut -d: -f2 | cut -d ' ' -f1 | sha1sum  | cut -d ' ' -f 1 | cut -c 1-8`
 
-hostnamectl set-hostname $NEW_HOSTNAME
-sed -i "s/$OLD_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+if [ $OLD_HOSTNAME != $NEW_HOSTNAME ]
+then
+  hostnamectl set-hostname $NEW_HOSTNAME
+  sed -i "s/$OLD_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+else
+  echo "HOSTNAME unchanged"
+fi
+
